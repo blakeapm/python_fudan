316d2c1038af93a4ced9376e140d76613d1a9bee
Blake Miller <b.a.miller@lse.ac.uk>
2024-07-08 15:00:20 +0800

    Add simple multiproccessing example.

---
+++ day_6/multi_example.py
@@ -0,0 +1,16 @@
+import multiprocessing
+
+from multiprocessing import Pool
+
+def print_number(number):
+    print(number)
+
+data_list = list(range(10000))
+
+print("This is going to use one CPU core.")
+for d in data_list:
+	print_number(d)
+
+if __name__ == '__main__':
+	with Pool(multiprocessing.cpu_count()) as p:
+		p.map(print_number, data_list)
