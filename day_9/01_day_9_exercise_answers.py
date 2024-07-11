6cba42425874f2a452d67d3ba333d4d0a2860fa9
Blake Miller <b.a.miller@lse.ac.uk>
2024-07-11 14:51:51 +0800

    Add answers to day 9 exercise.

--- day_9/01_day_9_exercise_answers.py
+++ day_9/01_day_9_exercise_answers.py
@@ -13,7 +13,7 @@ and print the working directory with the command `pwd` paste the output in
 place of '/Users/your_username/Documents/python_fudan/day_9'
 '''
 
-working_dir = '/Users/your_username/Documents/python_fudan/day_9/'
+working_dir = '/Users/your_username/Documents/python_fudan/day_9'
 
 input_csv_fn = 'twitter_data.csv'
 input_fn = 'twitter_locations.txt'
@@ -26,7 +26,7 @@ output_fn = 'twitter_locations_clean.txt'
 ###############################################################################
 
 locations = []
-with open(working_dir + input_fn, 'r') as f:
+with open(working_dir + '/' + input_fn, 'r') as f:
     for line in f:
         word = line.strip()
         locations.append(word)
@@ -37,12 +37,21 @@ with open(working_dir + input_fn, 'r') as f:
 #                                                                             #
 ###############################################################################
 
+ca_pattern = r'\bC\w+\b\.?'
+wa_pattern = r'\bW\w+\b\.?'
+ma_pattern = r'\bM\w+\b\.?'
+
 ###############################################################################
 #                                                                             #
 #    4. Define a function to standardize the state names using your regex.    #
-#    Use CA, WA, MA as the standard text referring to state.			      #
 #                                                                             #
 ###############################################################################
+
+def standardize_state_name(location):
+    location = re.sub(ca_pattern, 'CA', location)
+    location = re.sub(wa_pattern, 'WA', location)
+    location = re.sub(ma_pattern, 'MA', location)
+    return location
 
 ###############################################################################
 #                                                                             #
@@ -50,12 +59,21 @@ with open(working_dir + input_fn, 'r') as f:
 #                                                                             #
 ###############################################################################
 
+standardized_locations = []
+for location in locations:
+    standardized_location = standardize_state_name(location)
+    standardized_locations.append(standardized_location)
+
 ###############################################################################
 #                                                                             #
 #    6. Write the standardized locations to output_file_path                  #
 #    ('twitter_locations_clean.txt').                                         #
 #                                                                             #
 ###############################################################################
+
+with open(output_fn, 'w') as f:
+    for location in standardized_locations:
+        f.write(location + '\n')
 
 ###############################################################################
 #                                                                             #
@@ -69,9 +87,26 @@ with open(working_dir + input_fn, 'r') as f:
 #                                                                             #
 ###############################################################################
 
+def standardize_state_name(location):
+    if location.startswith('C'):
+        return 'California'
+    elif location.startswith('W'):
+        return 'Washington'
+    elif location.startswith('M'):
+        return 'Massachusetts'
+    return None
+
 ###############################################################################
 #                                                                             #
 #    8. Now read in the `twitter_data.csv` file as a DataFrame in Pandas      #
 #    Print our the first and last 10 rows of the data frame.                  #
 #                                                                             #
 ###############################################################################
+
+import pandas as pd
+
+
+twitter_data = pd.read_csv(working_dir + '/' + input_csv_fn, index=False)
+
+twitter_data.head(10)
+twitter_data.tail(10)
